7fc2ca133f3d160ccae3617d5f15b5b0c32deb02
Eduard4002 <mihiceduard@gmail.com>
2023-09-13 14:54:31 +0200

    AddCellToPage function

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ var itemsGrid = document.getElementById("itemsGrid");
 
 function submitItem(){
     if(inputItem.value == "") return;
-    //Create HTML/CSS elements
-    var cell = document.createElement("div");
-    cell.className = "cell";
-    cell.innerHTML = inputItem.value;
-
-    itemsGrid.appendChild(cell);
+    addCellToPage(inputItem);
 
     inputItem.value = "";
 
     localStorage.setItem("Grid", itemsGrid.innerHTML);
+}
+function addCellToPage(itemName){
+    var cell = document.createElement("div");
+    cell.className = "cell";
+
+    var paragraph = document.createElement("p");
+    paragraph.innerHTML = inputItem.value;
+
+    cell.appendChild(paragraph);
+
+
+    itemsGrid.appendChild(cell);
 }
 function loadItems(){
     const grid = localStorage.getItem("Grid");
